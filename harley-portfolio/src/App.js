8c7d2752debf399c5f0686287af304a8c9cb4155
harley-rm <reevesmartin.harley@gmail.com>
2022-01-09 01:50:16 +0000

    Create very simple webpage, with some examples of react bootstrap componenets and styling

--- harley-portfolio/src/App.js
+++ harley-portfolio/src/App.js
@@ -1,22 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button, Alert, Breadcrumb, Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card style={{ color: "#000" }}>
+          <Card.Img />
+          <Card.Body>
+            <Card.Title>
+              Card Example
+            </Card.Title>
+            <Card.Text>
+              This is an example of react bootstrap cards!
+            </Card.Text>
+            <Button variant="primary">read more!</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test1</Breadcrumb.Item>
+          <Breadcrumb.Item>Test2</Breadcrumb.Item>
+          <Breadcrumb.Item active>Test3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Alert variant="success">This is a button</Alert>
+        <Button>Test Button</Button>
       </header>
     </div>
   );
