84656386791be188c24cd4efe40be0ae9de30acc
Harley Reeves-Martin <harley.reeves-martin@genomicsplc.com>
2022-02-28 16:07:08 +0000

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -2,33 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Button, Alert, Breadcrumb, Card } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Card style={{ color: "#000" }}>
-          <Card.Img />
-          <Card.Body>
-            <Card.Title>
-              Card Example
-            </Card.Title>
-            <Card.Text>
-              This is an example of react bootstrap cards!
-            </Card.Text>
-            <Button variant="primary">read more!</Button>
-          </Card.Body>
-        </Card>
-        <Breadcrumb>
-          <Breadcrumb.Item>Test1</Breadcrumb.Item>
-          <Breadcrumb.Item>Test2</Breadcrumb.Item>
-          <Breadcrumb.Item active>Test3</Breadcrumb.Item>
-        </Breadcrumb>
-        <Alert variant="success">This is a button</Alert>
-        <Button>Test Button</Button>
-      </header>
-    </div>
+    <Router>
+        
+    </Router>
   );
 }
 
